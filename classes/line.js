3635b0a00d27e8ec3702104692d25a345280e1d3
alexbol99 <alexbol99@gmail.com>
2017-02-20 22:30:18 +0200

    Line class started

---
+++ classes/line.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Alex Bol on 2/20/2017.
+ */
+"use strict";
+
+let RegisterLine = function(Flatten) {
+    Flatten.Line = class Line {
+
+    }
+};
+
+module.exports = RegisterLine;
