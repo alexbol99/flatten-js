984251ad9ed5ab8872e671eefb93f91fee24d0fd
alexanderbol <alexbol99@gmail.com>
2023-12-01 19:50:44 +0200

    Fix export module entry point - target main.mjs instead main.esm

--- rollup.config.js
+++ rollup.config.js
@@ -4,12 +4,12 @@ export default {
     input: 'index.js',
     output: [
         {
-            file: 'dist/main.cjs.js',
+            file: 'dist/main.cjs',
             format: 'cjs',
             exports: 'named'
         },
         {
-            file: 'dist/main.esm.js',
+            file: 'dist/main.mjs',
             format: 'esm',
             exports: 'named'
         },
